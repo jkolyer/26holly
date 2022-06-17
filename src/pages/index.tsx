420dbcf842451a76c4f99538379f94ae6096149b
Jonathan Kolyer <jonathankolyer@gmail>
2022-06-17 11:28:16 -0400

    setup section data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,13 @@ import * as React from "react"
 // styles
 const pageStyles = {
   color: "#232129",
-  padding: 96,
+  padding: 48,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
-  maxWidth: 320,
+  maxWidth: 480,
 }
 const headingAccentStyles = {
   color: "#663399",
@@ -61,19 +61,6 @@ const descriptionStyle = {
   lineHeight: 1.25,
 }
 
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
-  }
-]
-
 const badgeStyle = {
   color: "#fff",
   backgroundColor: "#088413",
@@ -91,6 +78,23 @@ const badgeStyle = {
 }
 
 // data
+const sections = [
+  {
+    id: "bikes",
+    name: "Bikes",
+    color: "#E95800",
+    description: "Two bicycles are provided.  They are 'Fire Island' bikes.",
+    badge: false,
+    subsections: [
+      {
+        id: "locks",
+        name: "Locks",
+        description: "Use the provided locks when leaving the bike unattended.  Combination is found on info sheet within the house."
+      }
+    ]
+  }
+]
+
 const links = [
   {
     text: "Tutorial",
@@ -141,53 +145,63 @@ const links = [
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
-      <title>Home Page</title>
+      <title>26 Holly Home Guide</title>
       <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
         🎉🎉🎉
+        <br />
+        Welcome to 26 HOLLY WALK
+        🎉🎉🎉
+        <br />
+        <span style={headingAccentStyles}>Here's a simple guide to our home.</span>
       </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
       <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
+        {sections.map(section => (
+          <li key={section.id} style={{ ...listItemStyles, color: section.color }}>
             <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
+              {section.name}
+              {section.badge && (
                 <span style={badgeStyle} aria-label="New Badge">
                   NEW!
                 </span>
               )}
-              <p style={descriptionStyle}>{link.description}</p>
+              <p style={descriptionStyle}>{section.description}</p>
             </span>
+            <ul style={listStyles}>
+              {section.subsections.map(subsec => (
+                <li key={subsec.id}>
+                  {subsec.name}
+                  <p style={descriptionStyle}>{subsec.description}</p>
+                </li>
+              ))}
+            </ul>
           </li>
         ))}
       </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+      
+      {/* <ul style={listStyles}>
+          {links.map(link => (
+          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
+          <span>
+          <a
+          style={linkStyle}
+          href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
+          >
+          {link.text}
+          </a>
+          {link.badge && (
+          <span style={badgeStyle} aria-label="New Badge">
+          NEW!
+          </span>
+          )}
+          <p style={descriptionStyle}>{link.description}</p>
+          </span>
+          </li>
+          ))}
+          </ul>
+          <img
+          alt="Gatsby G Logo"
+          src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
+          /> */}
     </main>
   )
 }
